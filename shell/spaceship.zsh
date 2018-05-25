5bb9fd707ad4944f4701af53629eca796d13ff73
Austin Kim <austink@naehas.com>
2018-05-25 14:07:41 -0700

    cleaned up zshrc, set antigen to autodl

---
+++ shell/spaceship.zsh
@@ -0,0 +1,20 @@
+export SPACESHIP_VI_MODE_SHOW=false
+export SPACESHIP_EXIT_CODE_SHOW=true
+export SPACESHIP_CHAR_SYMBOL="> "
+
+export SPACESHIP_PROMPT_ORDER=(
+time
+user
+host
+dir
+git
+package
+line_sep
+char
+)
+
+export SPACESHIP_RPROMPT_ORDER=(
+exit_code
+exec_time
+jobs
+)
